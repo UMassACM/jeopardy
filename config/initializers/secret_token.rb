60ef9aba985c815439f4efeb22d247e211c98854
Ryan McCann <mccann.ryan.m@gmail.com>
2012-03-14 21:49:25 -0400

    Rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jeopardy::Application.config.secret_token = 'e45be6d6db34734b57a586c2f9f0750d819b842d0efe5708c47b10b097420f8e412beb5ea2c9a10d5ae9da09682c04c554c6012cbbba92ed906d9ffb94beada0'
